c41566c67f9fac586eb22c84c646fcc49e65000e
Imesh Umayanga <imeshu@wso2.com>
2019-12-10 13:36:07 +0530

    Update components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/util/OAuth2Util.java
    Resolve git comment
    
    Co-Authored-By: Farasath Ahamed <mefarazath@gmail.com>

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/util/OAuth2Util.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/util/OAuth2Util.java
@@ -3095,7 +3095,7 @@ public class OAuth2Util {
             }
         }
         if (log.isDebugEnabled()) {
-            log.debug(String.format("Retrieved jwks uri %s from the service provider %s", jwksUri, clientId));
+            log.debug(String.format("Retrieved jwks uri: %s for the service provider associated with client_id: %s", jwksUri, clientId));
         }
         return jwksUri;
     }
